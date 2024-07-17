3fa21273c61fb5a04ee987c9a339ed600d1d7257
bekirhancioqlu <bekirhancioglu@icloud.com>
2024-07-17 15:44:56 +0300

    kategori eklendi

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./Home.css"
+import icon1 from "../Assets/Iteration-2-aseets/icons/1.svg"
+import icon2 from "../Assets/Iteration-2-aseets/icons/2.svg"
+import icon3 from "../Assets/Iteration-2-aseets/icons/3.svg"
+import icon4 from "../Assets/Iteration-2-aseets/icons/4.svg"
+import icon5 from "../Assets/Iteration-2-aseets/icons/5.svg"
+import icon6 from "../Assets/Iteration-2-aseets/icons/6.svg"
 export const Home = () => {
   return (
       <div className="home">
@@ -19,6 +25,39 @@ export const Home = () => {
          
          </div>
         </div>
+        <div className="kategori">
+
+         <div>
+          <img src= {icon1} />
+         <p>YENİ!Kore</p>
+         </div> 
+
+          <div>
+            <img src= {icon2} />
+          <p>Pizza</p>
+          </div>
+
+          <div>
+            <img src= {icon3} />
+          <p>Burger</p>
+          </div>
+
+          <div>
+            <img src= {icon4} />
+          <p>Kızartmalar</p>
+          </div>
+
+          <div>
+            <img src= {icon5} />
+          <p>Fast Food</p>
+          </div>
+
+          <div>
+            <img src= {icon6} />
+          <p>Gazlı İçecekler</p>
+          </div>
+
+           </div>
           
       </div>
   )
