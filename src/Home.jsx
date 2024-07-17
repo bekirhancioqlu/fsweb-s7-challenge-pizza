7a6cab695e1f13857d8af51848ff9055247cc2aa
bekirhancioqlu <bekirhancioglu@icloud.com>
2024-07-17 15:18:44 +0300

    ana sayfa resim yazı ve button eklendi

---
+++ src/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Home.css"
+export const Home = () => {
+  return (
+      <div className="home">
+        <div className=" banner"> 
+        
+         <div className="giris">
+
+          <div><p className="baslik">Teknolojik Yemekler</p>  </div>
+
+          <div className="yazi"> 
+          <p className="aciklama">fırsatı kaçırma</p>
+          <p className="slogan">KOD ACIKTIRIR<br/> PIZZA, DOYURUR</p>
+          <button className="aciktim">Acıktım</button>
+
+          </div> 
+         
+         
+         </div>
+        </div>
+          
+      </div>
+  )
+}
+  
+ 
